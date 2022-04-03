392d02b37af48c3db2fa5d4940f977449d188d2c
mohammedbabelly <46360121+mohammedbabelly@users.noreply.github.com>
2022-04-03 13:11:00 +0300

    change the about section

--- lib/data/seeds/about_seeds.dart
+++ lib/data/seeds/about_seeds.dart
@@ -3,4 +3,4 @@ import 'package:mb_website/pages/about/controllers/about_controller.dart';
 AboutModel aboutSeeds = AboutModel(
     head: "Hello, my name is Mohammed Babelly.",
     about:
-        "I am an award-winning full-stack web developer and UI/UX javascript specialist.\nCheck out my articles, React and React Native UI components at the code laboratory.\nFeel free to take a look at my latest projects on the web portfolio page.\nRemotely available UTC−1 to UTC+8. rafael@caferati.me_");
+        "Flutter - BLoC - Redux - Provider - Stores deploy\nRESTful-API - GraphQL - Socket - SQL - MongoDB\n.NET core (Jason Taylor architecture)\nentity framework - Firebase - Git-FLow\nCI/CD - Azure Pipelines - Codemagic\nVPS hosting - nginx - Docker - Microservices\nPython - ML - DL - Tensorflow - CNN\n");
--- lib/pages/about/views/about_view.dart
+++ lib/pages/about/views/about_view.dart
@@ -14,36 +14,41 @@ class AboutView extends GetResponsiveView<AboutController> {
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Container(
-            width: 550,
-            height: 50,
-            alignment: Alignment.center,
-            child: HoverCard(
-                builder: (context, hovering) {
-                  return Text(controller.about.value.head,
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.montserrat(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w700,
-                          color: TextColors.head));
-                },
-                depth: 10,
-                onTap: () {},
-                depthColor: AppColors.purple,
-                shadow: BoxShadow(color: AppColors.purple, blurRadius: 50))),
+        // Container(
+        //     width: 550,
+        //     height: 50,
+        //     alignment: Alignment.center,
+        //     child: HoverCard(
+        //         builder: (context, hovering) {
+        //           return
+        Text(controller.about.value.head,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.montserrat(
+                fontSize: 25,
+                fontWeight: FontWeight.w700,
+                color: TextColors.head))
+        // ;
+        // },
+        // depth: 10,
+        // onTap: () {},
+        // depthColor: AppColors.purple,
+        // shadow: BoxShadow(color: AppColors.purple, blurRadius: 50)))
+        ,
         Container(
           margin: EdgeInsets.only(top: 12),
-          height: 200,
+          height: 250,
           child: TypewriterAnimatedTextKit(
             text: [controller.about.value.about],
             textAlign: TextAlign.center,
             textStyle: GoogleFonts.lato(
                 fontWeight: FontWeight.w400,
                 height: 2,
-                fontSize: 15,
+                fontSize: 18,
+                textStyle: TextStyle(wordSpacing: 3),
                 color: TextColors.body),
             speed: Duration(milliseconds: 50),
             repeatForever: false,
+            totalRepeatCount: 1,
             displayFullTextOnTap: true,
           ),
         )
