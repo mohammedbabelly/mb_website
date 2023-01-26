699d9a8a68ca33d5853185ae3d2ef160431ba854
Mohammed Babelly <mohammed@nevercode.io>
2023-01-26 19:15:32 +0400

    Change publish folder

--- lib/data/seeds/about_seeds.dart
+++ lib/data/seeds/about_seeds.dart
@@ -1,7 +1,7 @@
 import 'package:mb_website/pages/about/controllers/about_controller.dart';
 
 AboutModel aboutSeeds = AboutModel(
-    head: "Hey, I'm Mohammed Babelly.",
+    head: "Hey, I'm Mohammed Babelly 👋",
     about:
         "I'm a software engineer with 5 years of experience in the software industry.\nMy focus area for the past few years has been DevOps engineering,\nbut I'm also skilled at mobile development using Flutter."
     // "Flutter - BLoC - Redux - Provider - Stores deploy\nRESTful-API - GraphQL - Socket - SQL - MongoDB\n.NET core (Jason Taylor architecture)\nentity framework - Firebase - Git-FLow\nCI/CD - Azure Pipelines - Codemagic\nVPS hosting - nginx - Docker - Microservices\nPython - ML - DL - Tensorflow - CNN\n",
